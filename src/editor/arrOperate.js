96384e88a363417ecc219bff859adccab467106c
ArthurFree <lc940401@gmail.com>
2022-02-09 20:56:51 +0800

    feat: add array utils

--- src/editor/arrOperate.js
+++ src/editor/arrOperate.js
@@ -1,4 +1,5 @@
 const arrProperties = {};
+const objectProperties = {};
 
 arrProperties.m_each = (cb) => {
     let i = 0;
@@ -8,3 +9,37 @@ arrProperties.m_each = (cb) => {
         cb(this[i], i, this);
     }
 };
+
+objectProperties.m_each = function (cb) {
+    let i = 0;
+    const keys = Object.keys(this);
+    const { length } = keys;
+
+    for (; i < length; i++) {
+        cb(this[keys[i]], keys[i], this)
+    }
+}
+
+objectProperties.m_reduce = function (cb, memo) {
+    let i = 0;
+    const keys = Object.keys(this);
+    const { length } = keys;
+
+    for (; i < length; i++) {
+        memo = cb(memo, this[keys[i]], keys[i], this)
+    }
+
+    return memo;
+}
+
+
+function build(properties) {
+    return objectProperties.m_reduce.call(properties, (memo, value, key) => {
+        memo[key] = {
+            value: value,
+            configurable: true,
+        }
+
+        return memo;
+    }, {});
+}
