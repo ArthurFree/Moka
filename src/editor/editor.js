a85c3c419c45af81d18e3902cf26550ee8d4e4df
ArthurFree <lc940401@gmail.com>
2022-02-14 16:03:32 +0800

    delete: editor

--- src/editor/editor.js
+++
@@ -1,6 +0,0 @@
-import './arrOperate';
-import moka from "./core";
-import './watcher';
-import './editorUtils';
-
-export default moka;
--- src/editor/example/index.js
+++
@@ -1,5 +0,0 @@
-import moka from '../index';
-import markdownConversion from '../markdownConversion';
-
-markdownConversion.init();
-moka.init();
