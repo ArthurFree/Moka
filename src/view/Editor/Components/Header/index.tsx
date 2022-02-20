91e66684fec588c5b94acfd085b847222c4bd5d7
ArthurFree <lc940401@gmail.com>
2022-02-20 18:36:48 +0800

    feat: update editor area styles

--- src/view/Editor/Components/Header/index.tsx
+++ src/view/Editor/Components/Header/index.tsx
@@ -5,7 +5,9 @@ export default class Header extends React.Component {
     render(): React.ReactNode {
         return (
             <div className="header-wrap">
-                <div className="header-toggle-nav header-float-nav"></div>
+                <div className="header-toggle-nav header-float-nav">
+                    <i className="icon-menu" />
+                </div>
                 <div className="header-doc-path">Tech / Git / Git Commit 规范</div>
             </div>
         );
--- src/view/Editor/Components/Nav/index.tsx
+++ src/view/Editor/Components/Nav/index.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
 import './index.scss';
 
-export default class Nav extends React.Component {
-    render() {
+
+interface getNavStatus {
+    (status: string): void
+}
+
+interface getNavMode {
+    (mode: string): void
+}
+interface NavProps {
+    getNavStatus?: getNavStatus;
+    getNavMode?: getNavMode;
+};
+
+export default class Nav extends React.Component<NavProps> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 侧边栏展示模式 'float' or 'fixed'
+            mode: 'float',
+            // 浮窗模式(float)下，当前侧边栏展开(expand)或者收缩(close)的状态
+            status: 'expand'
+        };
+    }
+
+    navWrapEl = React.createRef<HTMLDivElement>();
+
+    navFloatEl = React.createRef<HTMLDivElement>();
+
+    handleChangeNavStatus = (status) => {
+        const { getNavStatus } = this.props;
+
+        this.setState({
+            status,
+        });
+
+        if (typeof getNavStatus === 'function') {
+            getNavStatus(status);
+        }
+    }
+
+    componentDidMount(): void {
+        this.navWrapEl.current.addEventListener('mouseenter', () => {
+            console.log('--- mouseenter ---');
+            this.handleChangeNavStatus('expand');
+        }, false);
+
+        this.navWrapEl.current.addEventListener('mouseleave', () => {
+            console.log('--- mouseleave ---');
+            this.handleChangeNavStatus('close');
+        }, false);
+    }
+
+    render(): React.ReactNode {
         return (
-            <div className="nav-wrap">
-                <div className="nav-menu-float nav-emnu-float-show">123123</div>
+            <div className="nav-wrap" ref={this.navWrapEl}>
+                <div className="nav-menu-float nav-emnu-float-show" ref ={this.navFloatEl}>123123</div>
             </div>
         );
     }
