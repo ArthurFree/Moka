38b4ba6275c14df4de1342d06159374351839ddb
ArthurFree <lc940401@gmail.com>
2022-03-08 14:43:14 +0800

    feat: toolbar change

---
+++ src/view/Editor/Components/EditorToolbar/Components/ToolbarGroup/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default class ToolbarGroup extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <div className="toolbar-group-wrap"></div>
+        );
+    }
+}
