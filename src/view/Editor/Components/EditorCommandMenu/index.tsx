e29de74a45ae0d92d1d92874a3dae1144fbfe5ae
ArthurFree <lc940401@gmail.com>
2022-03-22 20:49:43 +0800

    feat: init command menu component

---
+++ src/view/Editor/Components/EditorCommandMenu/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+export default class EditorCommandMenu extends React.Component {
+    render() {
+        return (
+            <div className="editor-command-menu-wrap">
+                <span>hello, world</span>
+            </div>
+        );
+    }
+}
